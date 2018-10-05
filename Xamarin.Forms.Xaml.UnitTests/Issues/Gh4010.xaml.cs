9da4b0e56a15ce70d4201599652b6212249d7809
Akihiko Odaki <nekomanma@pixiv.co.jp>
2018-10-05 21:04:51 +0900

    Test if a templated view keeps a name reference

---
+++ Xamarin.Forms.Xaml.UnitTests/Issues/Gh4010.xaml.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace Xamarin.Forms.Xaml.UnitTests
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Gh4010 : Element
+	{
+		public Xamarin.Forms.DataTemplate Template { get; set; }
+
+		public Gh4010()
+		{
+			InitializeComponent();
+		}
+
+		[TestFixture]
+		public static class Tests
+		{
+			[TestCase]
+			public static void PreserveName()
+			{
+				var i = new Gh4010();
+				var templated = (Gh4010)i.Template.CreateContent();
+				Assert.IsInstanceOf<Xamarin.Forms.DataTemplate>(templated.FindByName("template"));
+			}
+		}
+    }
+}
