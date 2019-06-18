0ba6ffe3b905a2ecd5390e64928cdfdf9bb5a5d6
Akihiko Odaki <nekomanma@pixiv.co.jp>
2019-06-18 11:33:30 +0900

    [XamlC] Do not try to set a value to bound source property if unavailable

--- Xamarin.Forms.Build.Tasks/SetPropertiesVisitor.cs
+++ Xamarin.Forms.Build.Tasks/SetPropertiesVisitor.cs
@@ -765,7 +765,13 @@ namespace Xamarin.Forms.Build.Tasks
 			else
 				il.Emit(Ldarg_0);
 			var locs = new Dictionary<TypeReference, VariableDefinition>();
-			il.Append(DigProperties(properties.Take(properties.Count - 1), locs, null, node as IXmlLineInfo, module));
+			Instruction pop = null;
+			il.Append(DigProperties(properties.Take(properties.Count - 1), locs, () => {
+				if (pop == null)
+					pop = Instruction.Create(Pop);
+
+				return pop;
+			}, node as IXmlLineInfo, module));
 
 			foreach (var loc in locs.Values)
 				setter.Body.Variables.Add(loc);
@@ -781,6 +787,12 @@ namespace Xamarin.Forms.Build.Tasks
 
 				il.Emit(Stloc, loc);
 				il.Emit(Ldloca, loc);
+			} else {
+				if (pop == null)
+					pop = Instruction.Create(Pop);
+
+				il.Emit(Dup);
+				il.Emit(Brfalse, pop);
 			}
 
 			if (lastIndexArg != null) {
@@ -802,6 +814,11 @@ namespace Xamarin.Forms.Build.Tasks
 				il.Emit(Call, setterRef);
 
 			il.Emit(Ret);
+
+			if (pop != null) {
+				il.Append(pop);
+				il.Emit(Ret);
+			}
 
 			context.Body.Method.DeclaringType.Methods.Add(setter);
 
